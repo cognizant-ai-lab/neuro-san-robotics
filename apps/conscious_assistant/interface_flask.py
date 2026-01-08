9d29ce60292a33e80dfd79ca1dd1b8ce8c2d53dd
Deepak Singh <deepak.singh10@cognizant.com>
2026-01-08 11:20:45 -0800

    try to get flask app to listening to LAN

--- apps/conscious_assistant/interface_flask.py
+++ apps/conscious_assistant/interface_flask.py
@@ -475,4 +475,12 @@ def run_scheduled_tasks():
 atexit.register(cleanup)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=False, port=5001, allow_unsafe_werkzeug=True, log_output=True, use_reloader=False)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5001,
+        debug=False,
+        allow_unsafe_werkzeug=True,
+        log_output=True,
+        use_reloader=False
+    )
